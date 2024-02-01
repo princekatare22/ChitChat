46c0bc30acc7629719718cae512b788499ffcadf
Prince Katare <75197980+princekatare22@users.noreply.github.com>
2024-02-02 04:32:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,14 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chit-chat-frontend-red.vercel.app",
     credentials: true,
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chit-chat-frontend-red.vercel.app",
     credentials: true,
   })
 );
