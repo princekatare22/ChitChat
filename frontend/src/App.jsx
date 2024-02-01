432221e3f7cf9d6a383c777e87a3d1376ee8f260
Prince Katare <75197980+princekatare22@users.noreply.github.com>
2024-02-02 04:56:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,7 @@ import { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 
 function App() {
-  const socket = useMemo(() => io("https://chit-chat-backend-five.vercel.app" ,{headers: {
-    "Content-Type" : "Access-Control-Allow-Origin"
-  }}), []);
+  const socket = useMemo(() => io("https://chit-chat-backend-five.vercel.app" ), []);
   
   const [Allmessage, setAllmessage] = useState([]);
   const [message, setMessage] = useState("");
