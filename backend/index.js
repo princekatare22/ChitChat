9828d7b1f96510881b367463c1468cc06e3e7577
Prince Katare <75197980+princekatare22@users.noreply.github.com>
2024-02-02 12:24:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,12 @@ const PORT = 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
- cors: {
-    origin: "https://chit-chat-frontend-red.vercel.app",
-    methods : ["POST", "GET"],
-    credentials: true,
-  }
-});
+const io = new Server(server, {cors: {origin: "*"}});
 
 app.use(
-  cors(
-    {
-    origin: "'https://chit-chat-frontend-red.vercel.app",
-    methods : ["POST", "GET"],
-    credentials: true,
-  }
-  )
+  cors({
+   origin: "*"
+  })
 );
 
 io.on("connection", function (socket) {
